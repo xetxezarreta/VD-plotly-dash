8a882bd894b4fff6b519f1f9f0aca5b8fd0599da
xetxezarreta <xabi1500@gmail.com>
2019-12-08 16:12:13 +0100

    sample graph

--- main.py
+++ main.py
@@ -90,7 +90,24 @@ app.layout = html.Div([
         ),
 
         # graph 1
-        html.Div([]),
+        html.Div([
+            #dcc.Graph(id='xai-graph'),
+            dcc.Graph(
+                id='example-graph',
+                figure={
+                    'data': [
+                        {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                        {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+                    ],
+                    'layout': {
+                        'title': 'Dash Data Visualization'
+                    }
+                },
+                className="box c"
+            ),                         
+        ],
+        id="xai",
+        ),
 
         # graph 2
         html.Div([]),
